495a2f698db2650592fd1bafd95b158476b349a5
Mac <164554792+gilberth-cabysis@users.noreply.github.com>
2025-03-28 19:44:07 -0600

    feat: FE changes for blogs Module

---
+++ front_changes/components/pages/blogs/NewBlog/NewBlog.tsx
@@ -0,0 +1,256 @@
+"use client";
+
+import React, { useRef, useState } from "react";
+import BubbleDecoration from "@/components/Common/BubbleDecoration";
+import Image from "next/image";
+import logo from "../../../../../public/images/hero/logo-repsell-icono.png";
+import {categorias} from "@/config/constants";
+import { api } from "@/utils/config";
+
+const NewBlog = () => {
+  const [message, setMessage] = useState("");
+  const [preview, setPreview] = useState(null);
+  const fileInputRef = useRef(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        const base64Image = reader.result;
+
+        if (typeof base64Image === "string") {
+          setPreview(base64Image);
+        }
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formDataPost = new FormData(e.target);
+
+      console.log(e.target);
+
+      formDataPost.append("image", preview);
+
+      const response = await fetch(
+        `${api}/blogs`,
+        {
+          method: "POST",
+          body: formDataPost,
+        },
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Blog creado exitosamente:", data);
+        setMessage("Blog creado exitosamente!");
+      } else {
+        console.error("Error al crear el blog");
+        setMessage("Error al crear el blog. Por favor, intenta nuevamente.");
+      }
+    } catch (error) {
+      console.error("Error al conectarse al backend:", error);
+      setMessage(
+        "Error al conectarse al backend. Por favor, intenta nuevamente.",
+      );
+    }
+  };
+
+  return (
+    <section
+      className="relative z-10 overflow-hidden py-24"
+      style={{
+        background: "radial-gradient(circle at top left, #1E3A8A 0%, #0A0F24 100%)",
+      }}
+    >
+      <BubbleDecoration/>
+
+      <div className="container">
+        <div className="-mx-4 flex flex-wrap justify-center">
+          <div className="w-full px-4 lg:w-10/12 xl:w-9/12">
+            <div className="mx-auto rounded-xl bg-[#101933]/60 px-6 py-10 text-white shadow-xl backdrop-blur-md sm:p-[60px]">
+              <Image
+                src={logo}
+                alt="logo"
+                width={50}
+                height={50}
+                style={{ width: 80, height: 80, margin: "auto", marginBottom: 20 }}
+              />
+              <h3 className="mb-3 text-center text-3xl font-bold drop-shadow">
+                AÑADIR NUEVO BLOG
+              </h3>
+              <p className="mb-10 mx-16 text-center text-white/80">
+                Completa los siguientes campos para crear una entrada de blog informativa, atractiva y alineada con los intereses de tus clientes.
+              </p>
+
+              <p className="text-lg font-bold text-center text-white/80">
+                Nota:
+              </p>
+
+              <p className="mb-10 text-center text-green-600">
+                ¡Recuerda para dividir en saltos de linea los parrafos y/o textos usar ( ; ) punto y coma!
+              </p>
+
+              <form onSubmit={handleSubmit}>
+                <div className="space-y-6">
+                  <div>
+                    <label className="block text-sm mb-2">Imagen:</label>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      onChange={handleFileChange}
+                      ref={fileInputRef}
+                      className="w-full rounded-md bg-[#1a1f33] px-4 py-3 text-white placeholder-white/50"
+                    />
+                  </div>
+
+                  {preview && (
+                    <div className="mb-6">
+                      <img src={preview} alt="Vista previa" className="mx-auto rounded-md shadow-md" />
+                    </div>
+                  )}
+
+                  {/* Category */}
+                  <div>
+                    <label className="block text-sm mb-2">Categoría:</label>
+                    <select
+                      name="category"
+                      className="w-full rounded-md bg-[#1a1f33] px-4 py-3 text-white"
+                    >
+                      {categorias.map((categoria) => (
+                        <option key={categoria.value} value={categoria.value}>
+                          {categoria.label}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+
+                  {/* Title */}
+                  <div>
+                    <label className="block text-sm mb-2">Título del Blog:</label>
+                    <input
+                      type="text"
+                      name="title"
+                      required
+                      placeholder="Ingresa un título"
+                      className="w-full rounded-md bg-[#1a1f33] px-4 py-3 text-white placeholder-white/50"
+                    />
+                  </div>
+
+                  {/* Intruduction */}
+                  <div>
+                    <label className="block text-sm mb-2">Introducción:</label>
+                    <textarea
+                      name="introduction"
+                      required
+                      className="w-full rounded-md bg-[#1a1f33] px-4 py-3 text-white placeholder-white/50"
+                      placeholder="Escribe la introducción"
+                    ></textarea>
+                  </div>
+
+                  {/* Subtitle 1 */}
+                  <div>
+                    <label className="block text-sm mb-2">Subtítulo 1:</label>
+                    <input
+                      type="text"
+                      name="subtitle1"
+                      required
+                      className="w-full rounded-md bg-[#1a1f33] px-4 py-3 text-white placeholder-white/50"
+                      placeholder="Ingresa un subtítulo"
+                    />
+                  </div>
+                  
+                  {/* Paragraph 1 */}
+                  <div>
+                    <label className="block text-sm mb-2">Párrafo:</label>
+                    <textarea
+                      name="paragraph1"
+                      className="w-full rounded-md bg-[#1a1f33] px-4 py-3 text-white placeholder-white/50"
+                      placeholder="Texto del párrafo"
+                    ></textarea>
+                  </div>
+                  
+                  {/* Subtitle 2 */}
+                  <div>
+                    <label className="block text-sm mb-2">Subtítulo 2:</label>
+                    <input
+                      type="text"
+                      name="subtitle2"
+                      className="w-full rounded-md bg-[#1a1f33] px-4 py-3 text-white placeholder-white/50"
+                      placeholder="Subtítulo adicional"
+                    />
+                  </div>
+                  
+                  {/* Paragraph 2 */}
+                  <div>
+                    <label className="block text-sm mb-2">Párrafo 2:</label>
+                    <textarea
+                      name="paragraph2"
+                      className="w-full rounded-md bg-[#1a1f33] px-4 py-3 text-white placeholder-white/50"
+                      placeholder="Segundo párrafo"
+                    ></textarea>
+                  </div>
+                  
+                  {/* Subtitle 3 */}
+                  <div>
+                    <label className="block text-sm mb-2">Conclusión:</label>
+                    <input
+                      name="conclusion"
+                      type="text"
+                      required
+                      className="w-full rounded-md bg-[#1a1f33] px-4 py-3 text-white placeholder-white/50"
+                      placeholder="Escribe la conclusión"
+                    />
+                  </div>
+
+                  {/* Paragraph 3 */}
+                  <div>
+                    <label className="block text-sm mb-2">Frase final:</label>
+                    <textarea
+                      name="paragraph3"
+                      required
+                      className="w-full rounded-md bg-[#1a1f33] px-4 py-3 text-white placeholder-white/50"
+                      placeholder="Frase o párrafo final"
+                    ></textarea>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="w-full rounded-full bg-[#e11b24] px-6 py-4 font-semibold text-white shadow-lg transition hover:bg-[#c8101c]"
+                  >
+                    Publicar Blog
+                  </button>
+
+                  {message && (
+                    <div
+                      className={`mt-6 text-center text-sm font-medium ${message.includes("exitosamente") ? "text-green-500" : "text-red-500"}`}
+                    >
+                      {message}
+                    </div>
+                  )}
+                </div>
+              </form>
+
+              <p className="mt-10 text-center text-sm text-white/80">
+                ¿Quieres ver tus blogs publicados? {" "}
+                <a href="/blog" className="text-[#4A6CF7] hover:underline">
+                  Ir al Blog
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NewBlog;
